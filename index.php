b69727db3f42ce4b6f7c0be62464ae62195bb0fe
Zachary Towell <zttowell@gmail.com>
2013-11-05 20:29:28 -0600

    Added layout

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Z&amp;O Web Services</title>
+<link rel="stylesheet" type="text/css" href="normalize.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="container">
+<div id="navbar">
+
+</div>
+<div id="photo">
+	<img src="/images/main.jpg" />
+</div>
+<div id="sidebar">
+
+</div>
+<div id="divider">
+
+</div>
+<div id="section0">
+
+</div>
+<div id="section1">
+
+</div>
+<div id="section2">
+
+</div>
+<div id="section3">
+
+</div>
+<div id="footer">
+
+</div>
+</div>
+</body>
+</html>
